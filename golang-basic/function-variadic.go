5d1a1ef23d9e04d1aba6fea5af28c84c317b34a3
Jauharuddin <jauharnotes@gmail.com>
2021-09-23 11:51:36 +0700

    lesson-11

--- golang-basic/function-variadic.go
+++ golang-basic/function-variadic.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func sumAll(number ...int) int {
 	total := 0
@@ -10,6 +13,13 @@ func sumAll(number ...int) int {
 	return total
 }
 
+func yourHobbies(name string, hobbies ...string) {
+	var hobbiesAsString = strings.Join(hobbies, ", ")
+
+	fmt.Printf("hello, my name is: %s\n", name)
+	fmt.Printf("my hobbies are: %s\n", hobbiesAsString)
+}
+
 func main() {
 	total := sumAll(10,10,10,10)
 	fmt.Println(total)
@@ -17,4 +27,11 @@ func main() {
 	slice := []int{10,20,30,40}
 	total = sumAll(slice...)
 	fmt.Println(total)
+
+	// Pengisian Parameter Fungsi Variadic Menggunakan Data Slice
+	var numbers = []int{2, 3, 4, 5, 6, 7, 8, 9, 10}
+	var avg = sumAll(numbers...)
+	fmt.Println("Rata-rata :", avg)
+
+	yourHobbies("johar", "reading", "traveler")
 }
