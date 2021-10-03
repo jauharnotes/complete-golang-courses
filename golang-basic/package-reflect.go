200a6f164d1588d43a6cff0fbc0359ec30fb33d1
Jauharuddin <jauharnotes@gmail.com>
2021-10-03 19:21:37 +0700

    lesson-15

--- golang-basic/package-reflect.go
+++ golang-basic/package-reflect.go
@@ -6,13 +6,32 @@ import (
 )
 
 type Sample struct {
-	Name string
+	Name string `requared:"true" max:"10"`
 }
 
 func main() {
 	sample := Sample{"Johar"}
-	sampleType := reflect.TypeOf(sample)
-	structField := sampleType.Field(0)
-	// required := structField.Tag.Get("requared")
-	fmt.Println(structField)
+	var sampleType reflect.Type = reflect.TypeOf(sample)
+	
+	fmt.Println(sampleType.NumField())
+	fmt.Println(sampleType.Field(0).Name)
+	fmt.Println(sampleType.Field(0).Tag.Get("requared"))
+	fmt.Println(sampleType.Field(0).Tag.Get("max"))
+
+	// sample.Name = "" 
+	fmt.Println(IsValid(sample))
+}
+
+func IsValid(data interface{}) bool {
+	t := reflect.TypeOf(data)
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		if field.Tag.Get("requared") == "true" {
+			value := reflect.ValueOf(data).Field(i).Interface()
+			if value == "" {
+				return false
+			}
+		}
+	}
+	return true
 }
