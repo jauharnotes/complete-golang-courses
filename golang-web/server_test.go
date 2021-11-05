f23c7ebbb34d4463e12e96723c441db0ca7e9cab
Jauharuddin <jauharnotes@gmail.com>
2021-11-05 23:27:20 +0700

    Update server_test.go

--- golang-web/server_test.go
+++ golang-web/server_test.go
@@ -15,3 +15,29 @@ func TestServer(t *testing.T) {
 		panic(err)
 	}
 }
+
+
+func TestServer(t *testing.T) {
+	//var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+	//	fmt.Fprintf(writer,"Test Server")
+	//}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer,"Home")
+	})
+	mux.HandleFunc("/server1", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer,"Server1")
+	})
+	mux.HandleFunc("/server2", func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writer,"Server2")
+	})
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
