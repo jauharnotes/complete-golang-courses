12e989045ff4149a3430b5953e08e0cf50dffcf7
Jauharuddin <jauharnotes@gmail.com>
2021-12-24 20:26:35 +0700

    golang web

---
+++ golang-web/handler_test.go
@@ -0,0 +1,40 @@
+package golang_web
+
+import(
+	"net/http"
+	"testing"
+	"fmt"
+)
+
+func TestHandler(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello, world!")
+	}
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestRequest(t *testing.T) {
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Method)
+		fmt.Fprint(w, r.RequestURI)
+	}
+
+	server := http.Server{
+		Addr: "localhost:8080",
+		Handler: handler,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
--- golang-web/server_test.go
+++ golang-web/server_test.go
@@ -1,7 +1,6 @@
 package golang_web
 
 import (
-	"fmt"
 	"net/http"
 	"testing"
 )
@@ -13,48 +12,6 @@ func TestServer(t *testing.T) {
 
 	err := server.ListenAndServe()
 	if err != nil {
-		panic(err)
-	}
-}
-
-
-func TestServer2(t *testing.T) {
-	//var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
-	//	fmt.Fprintf(writer,"Test Server")
-	//}
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer,"Home")
-	})
-	mux.HandleFunc("/image", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer,"image")
-	})
-	mux.HandleFunc("/image/thumnail", func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprint(writer,"thumnail")
-	})
-	server := http.Server{
-		Addr: "localhost:8080",
-		Handler: mux,
-	}
-
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func TestServerRequest(t *testing.T) {
-	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintln(writer, request.Method)
-		fmt.Fprintln(writer, request.RequestURI)
-	}
-	server := http.Server{
-		Addr: "localhost:8080",
-		Handler: handler,
-	}
-
-	err := server.ListenAndServe()
-	if err != nil{
 		panic(err)
 	}
 }
